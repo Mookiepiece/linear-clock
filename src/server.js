c53469ffa37dd2575572ba22d62fc27c31f6c423
彼術向 <48076971+Mookiepiece@users.noreply.github.com>
2021-01-22 08:43:49 +0800

    prettier

--- src/server.js
+++ src/server.js
@@ -29,11 +29,7 @@ server
         <meta charset="utf-8" />
         <title>Welcome to Razzle</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${
-          assets.client.css
-            ? `<link rel="stylesheet" href="${assets.client.css}">`
-            : ''
-        }
+        ${assets.client.css ? `<link rel="stylesheet" href="${assets.client.css}">` : ''}
         ${
           process.env.NODE_ENV === 'production'
             ? `<script src="${assets.client.js}" defer></script>`
